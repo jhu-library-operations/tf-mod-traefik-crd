41f3f31945f2d0541f5797d046e55cb46840a3f7
Derek Belrose <derek@derekbelrose.com>
2020-11-16 10:07:40 -0500

    Adding different output paths to comply with directory structure

--- main.tf
+++ main.tf
@@ -1,23 +1,25 @@
 resource "local_file" "crd" {
   content = file("${path.module}/templates/_crd.yaml")
-  filename = format("%s/crd.yaml", var.output_path)
+  filename = format("%s/crd.yaml", var.infrastructure_output_path)
   file_permission = "0600"
 }
 
 resource "local_file" "definitions" {
   content = file("${path.module}/templates/_definitions.yaml")
-  filename = format("%s/definitions.yaml", var.output_path)
+  filename = format("%s/definitions.yaml", var.infrastructure_output_path)
   file_permission = "0600"
 }
 
 resource "local_file" "resources" {
   content = file("${path.module}/templates/_resources.yaml")
-  filename = format("%s/resources.yaml", var.output_path)
+  filename = format("%s/resources.yaml", var.infrastructure_output_path)
   file_permission = "0600"
 }
 
 resource "local_file" "kustomize" {
   content = file("${path.module}/templates/_kustomization.yaml")
-  filename = format("%s/kustomization.yaml", var.output_path)
+  filename = format("%s/kustomization.yaml", var.infrastructure_output_path)
   file_permission = "0600"
 }
+
+
--- var.tf
+++ var.tf
@@ -1,3 +1,7 @@
-variable "output_path" {
+variable "infrastructure_output_path" {
+  type = string
+}
+
+variable "application_output_path" {
   type = string
 }
